1b8f44d17746cd38bea89991ccc12081cc3c238b
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-07-14 12:42:18 +0530

    updated models phase 1

---
+++ models/cart.models.js
@@ -0,0 +1,24 @@
+import mongoose,{Schema} from "mongoose";
+
+const CartSchema = new Schema(
+    {
+      userId:{
+        type: String,
+        required: true,
+      },
+      products:[
+        {
+            productId:{
+                type: String,
+            },
+            quantity:{
+                type: Number,
+                default: 1,
+            }
+        }
+      ]
+    },{timestamps: true}
+)
+
+
+export const Cart = mongoose.model("Cart",CartSchema)
