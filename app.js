6e640511308641f52d68c5325a6acdf89afb5829
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-07-22 02:00:39 +0530

    updated the app file to include all the routes

--- app.js
+++ app.js
@@ -18,6 +18,16 @@ app.use(cookieParser())
 
 
 import cartRouter from "./routes/cart.routes.js"
+import authRouter from "./routes/auth.routes.js"
+import userRouter from "./routes/user.routes.js"
+import productRouter from "./routes/product.routes.js"
+import orderRouter from "./routes/order.routes.js"
+
 app.use("/api/carts", cartRouter)
+app.use("/api/auth" ,  authRouter)
+app.use("/api/users", userRouter)
+app.use("/api/products", productRouter)
+app.use("/api/orders", orderRouter)
+
 
 export { app }
