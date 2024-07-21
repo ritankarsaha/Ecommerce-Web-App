a86ec995576d707789388f538b6a4acdb7743a36
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-07-22 02:02:30 +0530

    Updated the cart routes

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
-import { verifyJWT } from "../middlewares/auth.middleware";
-import { createCart, deleteCart, getAllCarts, getUserCart, updateCart } from "../controllers/cart.controller";
-
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { createCart, deleteCart, getAllCarts, getUserCart, updateCart } from "../controllers/cart.controller.js";
+import { verifyTokenAndAdmin,verifyTokenAndAuthorization,verifyToken } from "../middlewares/auth2.middleware.js";
 
 const router = Router()
 
 // listing all the cart routes over here.
 
-router.route("/").post(verifyJWT,createCart )
-router.route("/:id").put(verifyJWT,updateCart)
-router.route("/:id").delete(verifyJWT,deleteCart)
-router.route("/find/:userId").get(verifyJWT,getUserCart)
-router.route("/").get(verifyJWT,getAllCarts)
+router.route("/").post(verifyJWT ,createCart)
+router.route("/:id").put(verifyTokenAndAuthorization,updateCart)
+router.route("/:id").delete(verifyTokenAndAuthorization,deleteCart)
+router.route("/find/:userId").get(verifyTokenAndAuthorization,getUserCart)
+router.route("/").get(verifyTokenAndAdmin,getAllCarts)
 
 export default router
