0b504262a499630188a7974a8cc799d392cd0f65
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-07-21 12:23:36 +0530

    modified to display link more gracefully

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config({ path: './env' });
 connectDB()
 .then(() =>{
   app.listen(process.env.PORT || 8000 , () =>{
-      console.log(`Server is running at port ${process.env.PORT}`);
+      console.log(`Server is running at port http://localhost:${process.env.PORT}`);
   })
 })
 .catch((err) =>{
